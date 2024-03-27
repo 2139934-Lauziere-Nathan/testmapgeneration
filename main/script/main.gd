5d6936d5f9be0896971944d8d4efc74f004a1f53
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-03-27 13:16:25 -0700

    changement nom resources

--- main/script/main.gd
+++ main/script/main.gd
@@ -3,7 +3,7 @@ extends Node2D
  
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var newlv = load("res://map/scene/coeur.tscn").instantiate()
+	var newlv = load("res://map/structure_niveau/croix.tscn").instantiate()
 	add_child(newlv)
 	
 	pass # Replace with function body.
